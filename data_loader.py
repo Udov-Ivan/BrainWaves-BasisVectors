316fd60e215696766f5577457eb09a3b9a819cea
Heorhii <heorhii.martynenko.25@kse.org.ua>
2026-04-25 20:58:53 +0300

    added the resting eyes open dataset to the loader

--- data_loader.py
+++ data_loader.py
@@ -2,7 +2,7 @@ import mne
 from mne.datasets import eegbci
 
 
-edf_files = eegbci.load_data(subjects=[1], runs=[2]) #resting state (eyes closed)
+edf_files = eegbci.load_data(subjects=[1], runs=[1, 2]) #resting state (eyes closed)
 
 raw = mne.io.read_raw_edf(edf_files[0], preload=True)
 mne.datasets.eegbci.standardize(raw)
